fadbb4b86235bf0c7c43abcdd5f8825f4b9f9287
Hayden Freeman <mail@haydfree.com>
2026-03-30 07:35:42 -0500

    added dynamic OS functionality, should work with both openbsd and linux

--- dwmstatus.c
+++ dwmstatus.c
@@ -7,7 +7,12 @@ static Display *dpy;
 int get_date(char *const date) {
     int ret = -1;
 
-    FILE *fp = popen("date +\"%a %y/%m/%d %l:%M:%S %p\"", "r");
+    FILE *fp;
+    #ifdef __OpenBSD__
+    fp = popen("date +\"%a %y/%m/%d %l:%M:%S %p\"", "r");
+    #else
+    fp = popen("date +\"%a %y/%m/%d %I:%M:%S %p\"", "r");
+    #endif
     if (!fp) {
         fprintf(stderr, "get_date: got null fp.\n");
         goto cleanup;
@@ -28,7 +33,9 @@ cleanup:
 int get_tmp(char *tmp) {
     int ret = -1;
 
-    FILE *fp = popen("sysctl hw.sensors | awk -F'[ =]' 'NR==1 {print $2}'",
+    FILE *fp;
+    #ifdef __OpenBSD__
+    fp = popen("sysctl hw.sensors | awk -F'[ =]' 'NR==1 {print $2}'",
         "r");
     if (!fp) {
         fprintf(stderr, "get_tmp: got null fp.\n");
@@ -36,6 +43,16 @@ int get_tmp(char *tmp) {
     }
     fgets(tmp, BUFSIZE, fp);
     tmp[strcspn(tmp, ".")] = '\0';
+    #else
+    fp = popen("cat /sys/class/thermal/thermal_zone0/temp", "r");
+    if (!fp) {
+        fprintf(stderr, "get_tmp: got null fp.\n");
+        goto cleanup;
+    }
+    fgets(tmp, BUFSIZE, fp);
+    int a = atoi(tmp)/1000;
+    snprintf(tmp, BUFSIZE, "%d", a);
+    #endif
 
     ret = 0;
 cleanup:
@@ -44,16 +61,33 @@ cleanup:
     return ret;
 }
 
-int get_ram(char *ram) {
+int get_mem(char *mem) {
     int ret = -1;
 
-    FILE *fp = popen("vmstat | awk 'NR==3 {print $3}'", "r");
+    FILE *fp;
+    #ifdef __OpenBSD__
+    fp = popen("vmstat | awk 'NR==3 {print $3}'", "r");
     if (!fp) {
-        fprintf(stderr, "get_ram: got null fp.\n");
+        fprintf(stderr, "get_mem: got null fp.\n");
         goto cleanup;
     }
-    fgets(ram, BUFSIZE, fp);
-    ram[strcspn(ram, "\n")] = '\0';
+    fgets(mem, BUFSIZE, fp);
+    mem[strcspn(mem, "\n")] = '\0';
+    #else
+    fp = popen("cat /proc/meminfo | awk 'NR==1 || NR==3 {print $2}'", "r");
+    if (!fp) {
+        fprintf(stderr, "get_mem: got null fp.\n");
+        goto cleanup;
+    }
+    int total, available, used;
+    char str_total[BUFSIZE], str_available[BUFSIZE];
+    fgets(str_total, BUFSIZE, fp);
+    fgets(str_available, BUFSIZE, fp);
+    total = atoi(str_total);
+    available = atoi(str_available);
+    used = (total - available) / 1024;
+    snprintf(mem, BUFSIZE, "%dM", used);
+    #endif
 
     ret = 0;
 cleanup:
@@ -85,14 +119,35 @@ int get_bat(char *bat) {
     int ret = -1;
 
     char status[BUFSIZE], capacity[BUFSIZE];
-    FILE *fp = popen("apm -v | awk 'NR==1 {print $4} NR==2 {print $4}'",
+    FILE *fp;
+    #ifdef __OpenBSD__
+    fp = popen("apm -v | awk 'NR==1 {print $4} NR==2 {print $4}'",
         "r");
+    if (!fp) {
+        fprintf(stderr, "get_bat: got null fp.\n");
+        goto cleanup;
+    }
     fscanf(fp, "%s %s", capacity, status);
     if (!strncmp(status, "connected", 9))
         strncpy(status, "+", BUFSIZE);
     else
         strncpy(status, "-", BUFSIZE);
     snprintf(bat, BUFSIZE, "%s%s", status, capacity);
+    #else
+    fp = popen("cat /sys/class/power_supply/BAT0/capacity && cat /sys/class/power_supply/BAT0/status", "r");
+    if (!fp) {
+        fprintf(stderr, "get_bat: got null fp.\n");
+        goto cleanup;
+    }
+    fgets(capacity, BUFSIZE, fp);
+    capacity[strcspn(capacity, "\n")] = '\0';
+    fgets(status, BUFSIZE, fp);
+    if (!strncmp(status, "Charging", 8))
+        strncpy(status, "+", BUFSIZE);
+    else
+        strncpy(status, "-", BUFSIZE);
+    snprintf(bat, BUFSIZE, "%s%s%%", status, capacity);
+    #endif
 
     ret = 0;
 cleanup:
@@ -105,16 +160,29 @@ int get_net(char *net, char *netint) {
     int ret = -1;
 
     char cmd[BUFSIZE], status[BUFSIZE], ssid[BUFSIZE];
+    FILE *fp;
+    #ifdef __OpenBSD__
     snprintf(cmd, BUFSIZE,
         "ifconfig %s | awk 'NR==6 {v=$2} NR==7 {print v, $3}'",
         netint);
-    FILE *fp = popen(cmd, "r");
+    fp = popen(cmd, "r");
     fscanf(fp, "%s %s", status, ssid);
     if (!strncmp(status, "active", 6))
         strncpy(status, "+", BUFSIZE);
     else
         strncpy(status, "-", BUFSIZE);
+    #else
+    snprintf(cmd, BUFSIZE, "iw dev %s link | awk 'NR==1 {v=$1} NR==2 {print v, $2}'", netint);
+    fp = popen(cmd, "r");
+    fscanf(fp, "%s %s", status, ssid);
+    if (!strncmp(status, "Connected", 9))
+        strncpy(status, "+", BUFSIZE);
+    else {
+        strncpy(status, "-", BUFSIZE);
+        strncpy(ssid, "None", BUFSIZE);
+    }
     snprintf(net, BUFSIZE, "%s%s", status, ssid);
+    #endif
 
     ret = 0;
 cleanup:
@@ -127,7 +195,8 @@ int get_vol(char *vol) {
     int ret = -1;
 
     char status[BUFSIZE], level[BUFSIZE];
-    FILE *fp = popen(
+    FILE *fp;
+    fp = popen(
         "pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}'", "r");
     fgets(level, BUFSIZE, fp);
     pclose(fp);
@@ -176,9 +245,9 @@ int main(int argc, char **argv) {
             goto cleanup;
         }
 
-        char ram[BUFSIZE];
-        if (get_ram(ram)) {
-            fprintf(stderr, "main: cannot get ram.\n");
+        char mem[BUFSIZE];
+        if (get_mem(mem)) {
+            fprintf(stderr, "main: cannot get mem.\n");
             goto cleanup;
         }
 
@@ -209,7 +278,7 @@ int main(int argc, char **argv) {
         char status[BUFSIZE*10];
         snprintf(status, BUFSIZE*10,
             " vol: %s | net: %s | bat: %s | dsk: %s | mem: %s | tmp: %sC | %s ",
-            vol, net, bat, dsk, ram, tmp, date);
+            vol, net, bat, dsk, mem, tmp, date);
         XStoreName(dpy, DefaultRootWindow(dpy), status);
         XSync(dpy, False);
         sleep(1);
